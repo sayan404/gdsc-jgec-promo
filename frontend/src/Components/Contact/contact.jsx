4982503262954acc5c20f86d12ab706c687db6c2
sayan404 <95490367+sayan404@users.noreply.github.com>
2023-08-09 21:55:15 +0530

    added sendmail feature
    added sendmail feature

--- frontend/src/Components/Contact/contact.jsx
+++ frontend/src/Components/Contact/contact.jsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState , useRef } from 'react'
 import Header from '../Layout/header'
+import emailjs from '@emailjs/browser';
 import './contact.css'
 import { Link } from 'react-router-dom'
 import { Button } from '@mui/material'
@@ -11,9 +12,8 @@ import YouTubeIcon from '@mui/icons-material/YouTube';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import AOS from 'aos'
 import 'aos/dist/aos.css'
-
 const Contact = () => {
-
+    const form = useRef();
     useEffect(() => {
         AOS.init()
     }, [])
@@ -22,6 +22,21 @@ const Contact = () => {
     const [senderEmail, setSenderEmail] = useState('')
     const [senderSubject, setSenderSubject] = useState('')
     const [senderMessage, setSenderMessage] = useState('')
+
+  const sendEmail = (e) => {
+    e.preventDefault()
+    emailjs.sendForm(import.meta.env.VITE_YOUR_SERVICE_ID, import.meta.env.VITE_YOUR_TEMPLATE_ID , form.current , import.meta.env.VITE_YOUR_PUBLIC_KEY)
+      .then((result) => {
+          console.log(result.text);
+          setSenderName('');
+          setSenderEmail('');
+          setSenderSubject('');
+          setSenderMessage('');
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
     return (
         <div>
             <Header highLight={highLight} />
@@ -31,22 +46,22 @@ const Contact = () => {
                     <div className='contactText'>
                         <span className='contact'>Lets Get in Touch</span>
                     </div>
-                    <form>
+                    <form ref={form} onSubmit={sendEmail}>
                         <div className='formName'>
                             <p>Name</p>
-                            <input type='text' value={senderName} placeholder='Enter Your Name' onChange={(e) => setSenderName(e.target.value)} />
+                            <input type='text' name='user_name' value={senderName} placeholder='Enter Your Name' onChange={(e) => setSenderName(e.target.value)} />
                         </div>
                         <div className='formEmail'>
                             <p>Email</p>
-                            <input type='text' value={senderEmail} placeholder='Enter Your Email' onChange={(e) => setSenderEmail(e.target.value)} />
+                            <input type='email' name='user_email' value={senderEmail} placeholder='Enter Your Email' onChange={(e) => setSenderEmail(e.target.value)} />
                         </div>
                         <div className='formSubject'>
                             <p>Subject</p>
-                            <input type='text' value={senderSubject} placeholder='Enter Your Subject' onChange={(e) => setSenderSubject(e.target.value)} />
+                            <input type='text' name='subject' value={senderSubject} placeholder='Enter Your Subject' onChange={(e) => setSenderSubject(e.target.value)} />
                         </div>
                         <div className='formSubject'>
                             <p>Message</p>
-                            <input type='text' contenteditable value={senderMessage} placeholder='Enter Your Message' onChange={(e) => setSenderMessage(e.target.value)} />
+                            <input type='text' name='message' contenteditable value={senderMessage} placeholder='Enter Your Message' onChange={(e) => setSenderMessage(e.target.value)} />
                         </div>
                         <div className='moreEvents'>
                             <Button type='submit' endIcon={<SendIcon />} variant='contained' sx={{ width: '10vw', height: '7vh', padding :' 2vh 10vh' , margin: '0 auto', font: '2.5vh JetBrains Mono' }}>Submit</Button>
