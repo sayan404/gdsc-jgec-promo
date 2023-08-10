4e1a1e66e85e0c92a8d9f2825ef1382ebb960946
sayan404 <95490367+sayan404@users.noreply.github.com>
2023-08-10 23:48:57 +0530

    now the speedDial code is working
    now the speedDial code is working

--- frontend/src/Components/Layout/header.jsx
+++ frontend/src/Components/Layout/header.jsx
@@ -1,108 +1,107 @@
-import React, { useEffect, useState } from 'react'
-import './header.css'
-import gdscLogo from '../../assets/gdsc.jpeg'
-import { Link } from 'react-router-dom'
-import HomeIcon from '@mui/icons-material/Home'
+import React, { useEffect, useState } from 'react';
+import './header.css';
+import gdscLogo from '../../assets/gdsc.jpeg';
+import { Link } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
 import CallIcon from '@mui/icons-material/Call';
-import Box from '@mui/material/Box'
-import SpeedDial from '@mui/material/SpeedDial'
-import SpeedDialAction from '@mui/material/SpeedDialAction'
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
 import InfoIcon from '@mui/icons-material/Info';
 import EventIcon from '@mui/icons-material/Event';
 import AccountTreeIcon from '@mui/icons-material/AccountTree';
 import Diversity3Icon from '@mui/icons-material/Diversity3';
-import { useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom';
+import Backdrop from '@mui/material/Backdrop';
 const Header = ({ highLight }) => {
-  const navigate = useNavigate()
-  const [open, setOpen] = useState(true)
+  const navigate = useNavigate();
+  const [open, setOpen] = useState(false); // Initialize the speed dial as closed
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  const actionsForLoogedInUsers = [
+  const actionsForLoggedInUsers = [
     { icon: <HomeIcon />, name: 'Home', func: home },
-    { icon: <InfoIcon />, name: 'about', func: about },
-    { icon: <EventIcon />, name: 'event', func: events },
-    { icon: <AccountTreeIcon />, name: 'projects', func: projects },
-    { icon: <Diversity3Icon />, name: 'team', func: team },
+    { icon: <InfoIcon />, name: 'About', func: about },
+    { icon: <EventIcon />, name: 'Events', func: events },
+    { icon: <AccountTreeIcon />, name: 'Projects', func: projects },
+    { icon: <Diversity3Icon />, name: 'Team', func: team },
     { icon: <CallIcon />, name: 'Contact', func: contact },
+  ];
 
-  ]
   function home() {
-    navigate('/home')
+    navigate('/home');
   }
   function about() {
-    navigate('/about')
+    navigate('/about');
   }
   function events() {
-    navigate('/events')
+    navigate('/events');
   }
   function projects() {
-    navigate('/projects')
+    navigate('/projects');
   }
   function team() {
-    navigate('/team')
+    navigate('/team');
   }
   function contact() {
-    navigate('/contact')
-  }
-  const showMenuHandler = () => {
-    open ? setOpen(false) : setOpen(true)
+    navigate('/contact');
   }
 
-  
+  const showMenuHandler = () => {
+    setOpen(!open); // Toggle the state of the speed dial
+  };
+
   useEffect(() => {
-    // Update window width when the window is resized
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
     };
-    // handleResize()
+
     window.addEventListener('resize', handleResize);
 
-    // Clean up the event listener when the component is unmounted
-    // return () => {
-    //   window.removeEventListener('resize', handleResize);
-    // };
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
   }, []);
+
   return (
     <>
-      {
-        (windowWidth === 600) ?
-         ( <Box sx={{ height: '100%', zIndex : '5', transform: 'translateZ(0px)', flexGrow: 1 }} className='speedDial'>
-            <SpeedDial
-              ariaLabel="SpeedDial basic example"
-              className='speedDialIcon' alt='Profile'
-              onClick={showMenuHandler}
-              open={open}
-              direction='down'
-            >
-              {actionsForLoogedInUsers.map((action) => (
-                <SpeedDialAction
-                  key={action.name}
-                  icon={action.icon}
-                  tooltipTitle={action.name}
-                  onClick={action.func}
-                />
-              ))}
-            </SpeedDial>
-          </Box>)
-          : (<div className='headerMainContainer'>
-            <div className='leftSecton'>
-              <img src={gdscLogo} className='gdscLogo' />
-            </div>
-            <div className='rightSecton'>
-              <ul>
-                <Link to={'/home'}><p className={(highLight === 'Home') ? "Home" : ""} >Home</p></Link>
-                <Link to={'/about'}><p className={(highLight === 'About') ? "About" : ""} >About</p></Link>
-                <Link to={'/events'}><p className={(highLight === 'Events') ? "Events" : ""} >Events</p></Link>
-                <Link to={'/projects'}><p className={(highLight === 'Projects') ? "Projects" : ""} >Projects</p></Link>
-                <Link to={'/team'}><p className={(highLight === 'Team') ? "Team" : ""} >Team</p></Link>
-                <Link to={'/contact'}><p className={(highLight === 'Contact') ? "Contact" : ""} >Contact</p></Link>
-              </ul>
-            </div>
-          </div>)
-
+      {windowWidth <= 600 ? (<> <Backdrop open={open} style={{ zIndex: "4" }} />
+      <img src={gdscLogo} className='gdscLogo mediaquery' />
+            <Box sx={{ height: '100%', transform: 'translateZ(0px)', flexGrow: 1 }} className='speedDial'>
+                <SpeedDial
+                    ariaLabel="SpeedDial basic example"
+                    onClick={showMenuHandler}
+                    open={open}
+                    direction='down'
+                    // className='speedDialOrg'               
+                >
+                    {actionsForLoggedInUsers.map((action) => (
+                        <SpeedDialAction
+                            key={action.name}
+                            icon={action.icon}
+                            tooltipTitle={action.name}
+                            onClick={action.func}
+                        />
+                    ))}
+                </SpeedDial>
+            </Box>
+        </>) : (<div className='headerMainContainer'>
+        <div className='leftSecton'>
+          <img src={gdscLogo} className='gdscLogo' />
+        </div>
+        <div className='rightSecton'>
+          <ul>
+            <Link to={'/home'}><p className={(highLight === 'Home') ? "Home" : ""} >Home</p></Link>
+            <Link to={'/about'}><p className={(highLight === 'About') ? "About" : ""} >About</p></Link>
+            <Link to={'/events'}><p className={(highLight === 'Events') ? "Events" : ""} >Events</p></Link>
+            <Link to={'/projects'}><p className={(highLight === 'Projects') ? "Projects" : ""} >Projects</p></Link>
+            <Link to={'/team'}><p className={(highLight === 'Team') ? "Team" : ""} >Team</p></Link>
+            <Link to={'/contact'}><p className={(highLight === 'Contact') ? "Contact" : ""} >Contact</p></Link>
+          </ul>
+        </div>
+      </div>)
       }
     </>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
